afce9c361678c8025d4f9dc93262278fbf04dc9c
alleytuna <anais.altun@gmail.com>
2023-04-25 11:13:58 +0200

    update css name

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 
 var _react = _interopRequireDefault(require("react"));
 var _reactDom = _interopRequireDefault(require("react-dom"));
-var _Table = _interopRequireDefault(require("./lib/Table"));
+var _Table = _interopRequireDefault(require("./lib/Table.jsx"));
 var _sampleData = _interopRequireDefault(require("./data/sample-data.json"));
 var _jsxRuntime = require("react/jsx-runtime");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
