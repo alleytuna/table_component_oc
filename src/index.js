ae104b82bac21f130dcc43deda2596d8a52b6c9d
alleytuna <anais.altun@gmail.com>
2023-04-25 11:26:27 +0200

    clean up

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Table from './lib/Table.jsx';
+import data from "./data/sample-data.json"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <Table data={data} />
+    </div>
+  </React.StrictMode>
+);
